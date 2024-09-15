313ba317003659695fe79592a50a8747921cf2be
Chinseok Lee <me@pyhub.kr>
2024-09-15 20:40:02 +0900

    async stream 방식으로 LLM Chat API 호출

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,4 @@
-from asgiref.sync import async_to_sync
-from typing import List
+from typing import List, AsyncGenerator
 
 from django.core.files import File
 from django.http import HttpResponse
@@ -15,7 +14,7 @@ def index(request):
 
 
 @require_POST
-def message(request):
+async def message(request):
     form = MessageForm(data=request.POST,
                        files=request.FILES)
     if not form.is_valid():
@@ -30,20 +29,26 @@ def message(request):
     photos: List[File] = form.cleaned_data["photos"]
 
     vendor, model = "openai", "gpt-4o"
-    llm_response: LLMResponse = async_to_sync(make_llm_response)(
+    llm_stream_response: AsyncGenerator[LLMResponse, None] = await make_llm_response(
         vendor=vendor, model=model, system_prompt="", user_prompt=user_text,
-        chat_history=[], temperature=1.0, max_tokens=1024, stream=False, files=photos,
+        chat_history=[], temperature=1.0, max_tokens=1024, stream=True, files=photos,
     )
 
-    estimated_cost_usd = llm_response.get_cost_usd() or 0
+    assistant_text = ""
+    llm_chunk_response = LLMResponse()
+    async for llm_chunk_response in llm_stream_response:
+        if llm_chunk_response.text:
+            assistant_text += llm_chunk_response.text
+
+    estimated_cost_usd = llm_chunk_response.get_cost_usd() or 0
     exchange_rate = 1300  # 현재 환율을 가정
     estimated_cost_krw = estimated_cost_usd * exchange_rate
 
     # 템플릿 렌더링 추천 (render or render_to_string)
     return HttpResponse(f"""
         <p class="mb-2"><strong class="mr-1">사용자</strong><span class="text">{user_text}</span></p>
-        <p><strong class="mr-1">어시스턴트</strong><span class="text">{llm_response.text}</span></p>
-        <p class="mb-2 text-sm text-gray-500">입력 토큰: {llm_response.input_tokens},
-         출력 토큰: {llm_response.output_tokens},
+        <p><strong class="mr-1">어시스턴트</strong><span class="text">{assistant_text}</span></p>
+        <p class="mb-2 text-sm text-gray-500">입력 토큰: {llm_chunk_response.input_tokens},
+         출력 토큰: {llm_chunk_response.output_tokens},
          예상 비용: ${estimated_cost_usd:.4f} USD (약 {estimated_cost_krw:.4f} 원)</p>
     """)
