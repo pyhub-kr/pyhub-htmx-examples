1d67bdf82c8f134984031d5f46594ff2281366a8
Chinseok Lee <me@pyhub.kr>
2024-09-15 20:16:43 +0900

    장고앱 chat 생성 및 등록

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_cotton",
     "app",
+    "chat",
     "melon",
 ]
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,9 +6,11 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", include("app.urls")),
+    path("chat/", include("chat.urls")),
     path("melon/", include("melon.urls")),
     path("", TemplateView.as_view(
         template_name="root.html",
         extra_context={"ncp_map_client_id": settings.NCP_MAP_CLIENT_ID}
     ), name="root"),
 ]
+
