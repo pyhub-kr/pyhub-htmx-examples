29fcc83b3198488f642447322dcd5016092603a8
Chinseok Lee <me@pyhub.kr>
2024-09-15 23:14:28 +0900

    누락된 django-cors-headers 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "crispy_forms",
     # "crispy_bootstrap5",
     "crispy_tailwind",
@@ -59,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
